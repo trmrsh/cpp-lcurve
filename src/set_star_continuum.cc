70e1d3b87ae88fe16651e025579f05a2390ded06
Tom Marsh <t.r.marsh@warwick.ac.uk>
2014-04-23 23:51:03 +0100

    set_star_continuum -- spots modify temperatures

--- src/set_star_continuum.cc
+++ src/set_star_continuum.cc
@@ -19,7 +19,7 @@
  * 'back-heating'. The finite size of the source is included only
  * approximately.
  *
- * \param mdl   Model defining parameters
+ * \param mdl          Model defining parameters
  * \param star1        grid of elements over star 1, modified on exit
  * \param star2        grid of elements over star 2, modified on exit.
  */
@@ -56,15 +56,41 @@ void Lcurve::set_star_continuum(const Model& mdl,
         mdl.gravity_dark1 / Subs::dlpdlt(mdl.wavelength, mdl.t1);
 
     int nelem1 = star1.size();
+
+    // compute direction of star spot 11
+    Subs::Vec3 spot11;
+    bool is_spot11 = mdl.stsp11_long.defined && mdl.stsp11_lat.defined &&
+        mdl.stsp11_fwhm.defined && mdl.stsp11_tcen.defined;
+    double clong11=0., slong11=0., clat11=0., slat11=0.;
+    if(is_spot11){
+        clong11 = std::cos(Subs::deg2rad(mdl.stsp11_long.value));
+        slong11 = std::sin(Subs::deg2rad(mdl.stsp11_long.value));
+        clat11  = std::cos(Subs::deg2rad(mdl.stsp11_lat.value));
+        slat11  = std::sin(Subs::deg2rad(mdl.stsp11_lat.value));
+    }
+    spot11 = Subs::Vec3(clat11*clong11, clat11*slong11, slat11);
+
     for(int i=0; i<nelem1; i++){
         vec = cofm2 - star1[i].posn;
         r   = vec.length();
-        mu  = Subs::dot(star1[i].dirn,vec)/r;
+        mu  = Subs::dot(star1[i].dirn, vec)/r;
+
+        // compute unirradiated temperature allowing for
+        // offset from spot centre
+        double t1 = mdl.t1;
+        if(is_spot11){
+            double dist =
+                Subs::rad2deg(std::acos(Subs::dot(star1[i].posn, spot11)/
+                                        star1[i].posn.length()));
+            t1 += (mdl.stsp11_tcen-mdl.t1)*
+                std::exp(-Subs::sqr(dist/(mdl.stsp11_fwhm/Constants::EFAC))/2.);
+        }
+
         if(mu >= r2){
 
             // Full tilt irradiation
             geom = Subs::sqr(r2/r)*mu;
-            temp = pow(pow(mdl.t1*pow(double(star1[i].gravity),GDCBOL1),4)
+            temp = pow(pow(t1*pow(double(star1[i].gravity),GDCBOL1),4)
                        + mdl.absorb*pow(mdl.t2,4)*geom, 0.25);
         }else if(mu > -r2){
 
@@ -77,14 +103,14 @@ void Lcurve::set_star_continuum(const Model& mdl,
             geom = Subs::sqr(r2/r)*r2*(sqrt(1.-x0*x0)*(2+x0*x0)/3 -
                                        x0*(Constants::PI/2-asin(x0)))/
                 Constants::PI;
-            temp = pow(pow(mdl.t1*pow(double(star1[i].gravity),GDCBOL1),4)
+            temp = pow(pow(t1*pow(double(star1[i].gravity),GDCBOL1),4)
                        + mdl.absorb*pow(mdl.t2,4)*geom, 0.25);
 
         }else{
 
             // No irradiation
             geom = 0.;
-            temp = mdl.t1*pow(double(star1[i].gravity),GDCBOL1);
+            temp = t1*pow(double(star1[i].gravity),GDCBOL1);
 
         }
 
@@ -106,15 +132,41 @@ void Lcurve::set_star_continuum(const Model& mdl,
                                          Subs::abs(double(mdl.t2)));
 
     int nelem2 = star2.size();
+
+    // compute direction of star spot 21
+    Subs::Vec3 spot21;
+    bool is_spot21 = mdl.stsp21_long.defined && mdl.stsp21_lat.defined &&
+        mdl.stsp21_fwhm.defined && mdl.stsp21_tcen.defined;
+    double clong21=0., slong21=0., clat21=0., slat21=0.;
+    if(is_spot21){
+        clong21 = std::cos(Subs::deg2rad(mdl.stsp21_long.value));
+        slong21 = std::sin(Subs::deg2rad(mdl.stsp21_long.value));
+        clat21  = std::cos(Subs::deg2rad(mdl.stsp21_lat.value));
+        slat21  = std::sin(Subs::deg2rad(mdl.stsp21_lat.value));
+    }
+    spot21 = Subs::Vec3(clat21*clong21, clat21*slong21, slat21);
+
     for(int i=0; i<nelem2; i++){
         vec = cofm1 - star2[i].posn;
         r   = vec.length();
         mu  = Subs::dot(star2[i].dirn,vec)/r;
 
+        // compute unirradiated temperature allowing for
+        // offset from spot centre
+        double t2 = Subs::abs(double(mdl.t2));
+        if(is_spot21){
+            Subs::Vec3 off = star2[i].posn-cofm2;
+            double dist =
+                Subs::rad2deg(std::acos(Subs::dot(off, spot21)/
+                                        off.length()));
+            t2 += (mdl.stsp21_tcen-t2)*
+                std::exp(-Subs::sqr(dist/(mdl.stsp21_fwhm/Constants::EFAC))/2.);
+        }
+
         if(mu >= r1){
 
             geom = Subs::sqr(r1/r)*mu;
-            temp = pow(pow(Subs::abs(double(mdl.t2))*
+            temp = pow(pow(t2*
                            pow(double(star2[i].gravity),GDCBOL2),4)
                        + mdl.absorb*pow(mdl.t1,4.)*geom, 0.25);
 
@@ -124,15 +176,14 @@ void Lcurve::set_star_continuum(const Model& mdl,
             geom = Subs::sqr(r1/r)*r1*(sqrt(1.-x0*x0)*(2+x0*x0)/3
                                        - x0*(Constants::PI/2-asin(x0)))/
                 Constants::PI;
-            temp = pow(pow(Subs::abs(double(mdl.t2))*
+            temp = pow(pow(t2*
                            pow(double(star2[i].gravity),GDCBOL2),4)
                        + mdl.absorb*pow(mdl.t1,4.)*geom, 0.25);
 
         }else{
 
             geom = 0.;
-            temp = Subs::abs(double(mdl.t2))*pow(double(star2[i].gravity),
-                                                 GDCBOL2);
+            temp = t2*pow(double(star2[i].gravity), GDCBOL2);
         }
 
         star2[i].flux   = star2[i].area*Subs::planck(mdl.wavelength, temp);
